e8561de59abd380d0dc70223e07d389f3e4de55a
ParthCoder123 <89193161+ParthCoder123@users.noreply.github.com>
2021-08-19 15:33:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var song1 = "";
+var song2 = "";
+
+function preload(){
+    song1 = loadSound("Harry Potter.mp3");
+    song2 = loadSound("Dream Song.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600 , 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video , 0 , 0 , 600 , 500);
+}
+
+function playSong(){
+    
+}
